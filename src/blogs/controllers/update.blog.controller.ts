5fa17d279a6b1ac972df317f59e14561c36fff73
alexsharich <alexsharich@gmail.com>
2026-04-10 22:28:14 +0300

    clean code

--- src/blogs/controllers/update.blog.controller.ts
+++ src/blogs/controllers/update.blog.controller.ts
@@ -1,7 +1,5 @@
 import {Request, Response} from "express";
 import {blogsQueryRepository} from "../repositories/blogs.query.repository";
-import {BlogDto} from "../../routes/blogs-router/blogs.router";
-import {blogsCommandRepository} from "../repositories/blogs.command.repository";
 import {blogsService} from "../services/blogs.service";
 
 export const updateBlogController = async (req: Request, res: Response) => {//add 400
